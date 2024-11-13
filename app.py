974317d93f347ae7dae4f5876ab7b05183b5f3c5
Lukesh Rathore <lukeshrathore18@gmail.com>
2024-11-13 02:04:32 -0500

    Removed print statement.

--- app.py
+++ app.py
@@ -6,8 +6,6 @@ def main():
     env = sys.argv[1]
     db_details = DB_DETAILS[env]
     tables = get_tables('table_list')
-    for table in tables['table_name']:
-        print(table)
 
 
 if __name__ == '__main__':
